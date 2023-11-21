8662e460c553005f03d76264e346e3895ef82a12
David Sarmiento <edst83@gmail.com>
2023-11-20 20:14:21 -0600

    add graphql post to create event

--- src/components/register_form/index.tsx
+++ src/components/register_form/index.tsx
@@ -3,21 +3,53 @@ import './register_form.css';
 //COMPONENTS
 import NumericInputComponent from "../numeric_input_component"
 import { GreenSubmitButton } from '../green_submit_button';
-//EVENTS
-import axios from 'axios';
 
 function registerInBackernd(number: number, eventId: number){
-  
-  const response = axios.post('http://localhost:3001/api/v1/posts', {
-    username: number,
-    content: eventId
-  })
-  .then(function (response) {
-    alert(response);
-  })
-  .catch(function (error) {
-    alert(error);
-  });
+    
+  const graphqlEndpoint = 'http://127.0.0.1:3001/graphql';
+
+  // GraphQL query or mutation
+  const graphqlQuery = `
+    mutation CreateEvent {
+      createEvent(
+        input: {
+          userId: 1,
+          title: "shit fucker",
+          eventNumber: 5,
+          status: "on_hold",
+          datetime: "2022-10-26T17:30:00.000-04:00",
+          eventType: "athletic_race",
+        }
+      ) {
+          errors
+          clientMutationId
+      }
+    }
+  `;
+    
+  // Headers for the POST request
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+    
+  // Data to be sent in the request body
+  const data = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ query: graphqlQuery }),
+  };
+    
+  // Make the POST request using fetch
+  fetch(graphqlEndpoint, data)
+    .then(response => response.json())
+    .then(result => {
+      // Handle the GraphQL response here
+        console.log(result);
+    })
+    .catch(error => {
+      // Handle errors here
+      console.error('Error:', error);
+    });
 }
 
 function RegisterForm(){
