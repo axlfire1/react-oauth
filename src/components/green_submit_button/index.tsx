44d2189c68036a8ce6ae4a905008144389241503
David Sarmiento <edst83@gmail.com>
2023-11-21 23:34:59 -0600

    add id and boolean state as prop for button

--- src/components/green_submit_button/index.tsx
+++ src/components/green_submit_button/index.tsx
@@ -1,8 +1,16 @@
 import './green_submit_button.css'
+import React from 'react';
 
-function GreenSubmitButton(){
+interface ComponentProps {  
+  isSubmitDisabled: boolean;
+}
+ 
+const GreenSubmitButton: React.FC<ComponentProps> = ({isSubmitDisabled}) => {
+  console.log(isSubmitDisabled);
   return (
     <input
+      disabled={isSubmitDisabled}
+      id="greenButton"
       type="submit"
       value="Registrar"
       className='numeric_input'
