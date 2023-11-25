aff39db05e582564200d671f36721f901c568e6e
David Sarmiento <edst83@gmail.com>
2023-11-24 23:05:23 -0600

    update function

--- src/components/numeric_input_component/index.tsx
+++ src/components/numeric_input_component/index.tsx
@@ -1,12 +1,26 @@
 import './numeric_input_component.css';
 import React, { ChangeEvent, useState } from 'react';
+import { useContext } from 'react';
+import { globalContext, EmptyInput } from '../../contexts/form_context'
 
 const NumericInputComponent: React.FC = () => {
+  const enabled = useContext(globalContext);
+
+  if (!enabled) {
+    throw new Error('Global context not provided');
+  }
+  
   const [numericValue, setNumericValue] = useState<number | ''>('');
+
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const inputText = event.target.value;
     if (/^\d*$/.test(inputText)) { // Check if the input is a valid number
       setNumericValue(inputText === '' ? '' : parseInt(inputText, 10));
+    }
+    
+    // TODO FIX FUNCTION
+    if(inputText){
+      enabled.myFunction();
     }
   };
 
